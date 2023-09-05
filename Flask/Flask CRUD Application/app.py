afefb071a14165e83d55bd6c2184d6b783fee753
manoj <manojupadhya11@gmail.com>
2023-09-05 12:08:56 +0530

    flash messages added

--- Flask/Flask CRUD Application/app.py
+++ Flask/Flask CRUD Application/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template,redirect,request,url_for
+from flask import Flask, render_template,redirect,request,url_for,flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key = "flash message"
+
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -14,22 +16,35 @@ mysql = MySQL(app)
 
 @app.route('/')
 def Index():
-    return render_template('index.html')
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('index.html', students = data)
 
 
 
 @app.route('/insert', methods = ['POST'])
-def insert():
+def insert():    
     if request.method =="POST":
+        flash("Data Inserted Successfully")
+
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO students (name,email,phone) VALUES (%s, %s, %s)", {name,email,phone})
+        cur.execute("INSERT INTO students (name,email,phone) VALUES (%s, %s, %s)", (name,email,phone))
 
         mysql.connection.commit()
         return redirect(url_for('Index'))
+
+@app.route("/update", methods = ['PUT'])
+def update():
+    return
+
+
 
 if __name__ == '__main__':
     app.run()
